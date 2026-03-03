7448e5157947f20ec397184253bad63fd1422a34
Ejedge <elijahjob813@gmail.com>
2026-03-03 22:05:49 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadiusToAreaCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter radius: ");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            Double Area = ((double)radius * (double)radius) * Math.PI;
+            Double RoundedArea = Math.Round(Area, 2);
+            Console.WriteLine("Area: " + RoundedArea);
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
